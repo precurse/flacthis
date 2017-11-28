9dbcacee53f4d32881cf35d4a1b9d622a7bf3d52
Andrew Klaus <andrewklaus@gmail.com>
2017-11-27 20:51:09 -0700

    Adding new tests

---
+++ tests/conftest.py
@@ -0,0 +1,112 @@
+import pytest
+from flacthis import ConverterConfig, LosslessToLossyConverter
+import audio_codecs
+
+@pytest.fixture
+def converter_config():
+    config = ConverterConfig()
+    config.threads = 0
+    return config
+
+@pytest.fixture(scope='session')
+def create_mock_source_dir(tmpdir_factory):
+    import wave
+    import subprocess
+    import random
+    import struct
+    from shutil import copy2
+
+    # Setup directory structure
+    t_dir = tmpdir_factory.mktemp('flacthis_test_data')
+    src_dir = t_dir.join('src')
+    dest_dir = t_dir.join('dest')
+
+    # Make src directory
+    t_dir.mkdir('src')
+
+    # Make mock audio wav and flac files
+    sample_len = 1323000 # 10 seconds of noise
+
+    f_wav = src_dir.join('test_wav.wav')
+    f_flac = src_dir.join('test_flac.flac')
+
+    output = wave.open(str(f_wav), 'w')
+    values = []
+
+    output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
+
+    for i in range(0, sample_len/10):
+        value = random.randint(-32767, 32767)
+        packed_value = struct.pack('h', value)
+
+        # Duplicate some data to make it compressible
+        for i in range(0,10):
+            values.append(packed_value)
+            values.append(packed_value)
+
+    value_str = ''.join(values)
+    output.writeframes(value_str)
+    output.close()
+
+    # Compress wav file to flac
+    subprocess.call(['flac','--fast', str(f_wav), '-o', str(f_flac)])
+
+    # Create mock album folders
+    mock_albums = []
+
+    for i in range(0,3):
+        album = 'album_{}'.format(i)
+        src_dir.mkdir('album_{}'.format(i))
+        album_dir = src_dir.join(album)
+        mock_albums.append(album_dir)
+
+        # Copy mock audio files
+        copy2(str(f_wav), str(album_dir))
+        copy2(str(f_flac), str(album_dir))
+
+    # Create mock artwork
+    art_exts = LosslessToLossyConverter.artwork_ext
+    mock_art = []
+
+    for ext in art_exts:
+        for a in mock_albums:
+          art = a.join('art.' + ext)
+          open(str(art), 'a').close()
+          mock_art.append(art)
+
+    r = {
+        'mock_albums': mock_albums,
+        'mock_art': mock_art,
+        't_dir': t_dir,
+        'src_dir': src_dir,
+        'dest_dir': dest_dir
+    }
+
+    return r
+
+@pytest.fixture
+def codec_manager():
+    m = audio_codecs.CodecManager()
+    m.discover_codecs()
+
+    return m
+
+@pytest.fixture
+def flac_converter(converter_config, create_mock_source_dir, codec_manager):
+    conf = converter_config
+    conf.source_dir = str(create_mock_source_dir['src_dir'])
+    conf.dest_dir = str(create_mock_source_dir['dest_dir'])
+    conf.decoder = codec_manager.get_decoder('flac')
+    conv = LosslessToLossyConverter(conf)
+
+    return conv
+
+@pytest.fixture
+def wav_converter(converter_config, create_mock_source_dir, codec_manager):
+    conf = converter_config
+    conf.source_dir = str(create_mock_source_dir['src_dir'])
+    conf.dest_dir = str(create_mock_source_dir['dest_dir'])
+    conf.decoder = codec_manager.get_decoder('wav')
+    conv = LosslessToLossyConverter(conf)
+
+    return conv
--- tests/test_flacthis.py
+++ tests/test_flacthis.py
@@ -7,72 +7,124 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from flacthis import ConverterConfig, LosslessToLossyConverter
 import audio_codecs
 import multiprocessing
+import tempfile
 
 class TestConverterConfig(object):
-
     # Source dir tests
-    def test_source_dir_missing(self):
-        config = ConverterConfig()
+    def test_source_dir_missing(self, converter_config):
+        t_dir = tempfile.gettempdir()
         with pytest.raises(OSError, match='Source directory not a directory'):
-            config.source_dir = '/this/path_should/never/exist/9999999'
+            converter_config.source_dir = os.path.join(t_dir, 'path/does/not/exist')
 
-    def test_source_dir_is_file(self):
-        config = ConverterConfig()
+    def test_source_dir_is_file(self, converter_config):
         with pytest.raises(OSError, match='Source directory not a directory'):
-            config.source_dir = '/dev/null'
+            converter_config.source_dir = '/dev/null'
 
-    def test_source_dir_not_readable(self):
-        config = ConverterConfig()
+    def test_source_dir_not_readable(self, converter_config):
         with pytest.raises(OSError, match='Source directory not readable'):
-            config.source_dir = '/root/'
+            converter_config.source_dir = '/root/'
 
-    def test_source_dir_readable(self):
-        config = ConverterConfig()
-        config.source_dir = '/tmp/'
+    def test_source_dir_readable(self, converter_config):
+        converter_config.source_dir = '/tmp/'
 
     # Destination dir tests
-    def test_dest_dir_is_nonexistent_and_parent_not_writable(self):
-        config = ConverterConfig()
+    def test_dest_dir_is_nonexistent_and_parent_not_writable(self, converter_config):
         with pytest.raises(OSError, match='Destination directory parent not writable'):
-            config.dest_dir = '/this/path_should_not/exist_1234567890'
+            converter_config.dest_dir = '/this_path_is_not_writable'
 
-    def test_dest_dir_exists_and_not_writable(self):
-        config = ConverterConfig()
+    def test_dest_dir_exists_and_not_writable(self, converter_config):
         with pytest.raises(OSError, match='Destination directory not writable'):
-            config.dest_dir = '/'
+            converter_config.dest_dir = '/'
 
-    def test_dest_dir_exists_and_is_a_file(self):
-        config = ConverterConfig()
+    def test_dest_dir_exists_and_is_a_file(self, converter_config):
         with pytest.raises(OSError, match='Destination directory is a file'):
-            config.dest_dir = '/dev/null'
+            converter_config.dest_dir = '/dev/null'
 
-    def test_dest_dir_parent_writable(self):
-        config = ConverterConfig()
-        config.dest_dir = '/tmp/path_should/not_exist/999999'
+    def test_dest_dir_parent_writable(self, converter_config):
+        t_dir = tempfile.gettempdir()
+        dir = os.path.join(t_dir,'path_does_not_exist')
+        converter_config.dest_dir = dir
+        assert converter_config._dest_dir == dir
 
-    def test_dest_dir_writable(self):
-        config = ConverterConfig()
-        config.dest_dir = '/tmp/'
+    def test_dest_dir_writable(self, converter_config):
+        t_dir = tempfile.gettempdir()
+        converter_config.dest_dir = t_dir
+        assert converter_config._dest_dir == t_dir
 
-    def test_auto_thread_detection(self):
-        config = ConverterConfig()
-        config.threads = 0
+    def test_auto_thread_detection(self, converter_config):
+        converter_config.threads = 0
         cpu_count = multiprocessing.cpu_count()
 
-        assert config._threads == cpu_count
+        assert converter_config._threads == cpu_count
 
+class TestCodecManager(object):
+    def test_convert_invalid_encoder(self, codec_manager):
+        with pytest.raises(audio_codecs.SelectedCodecNotValid):
+            codec_manager.get_encoder('foo-bar-codec')
 
+    def test_convert_invalid_decoder(self, codec_manager):
+        with pytest.raises(audio_codecs.SelectedCodecNotValid):
+            codec_manager.get_decoder('foo-bar-codec')
 
+    def test_codec_manager_select_decoder_without_discover(self):
+        with pytest.raises(audio_codecs.SelectedCodecNotValid):
+            m = audio_codecs.CodecManager()
+            m.get_decoder('flac')
 
-# class TestConverter(unittest.TestCase):
-#     def setUp(self):
-#         self.config = ConverterConfig()
-#         self.config.source_dir = 'src'
-#         self.config.dest_dir = 'dest'
-#         self.converter = LosslessToLossyConverter(self.config)
+    def test_codec_manager_select_encoder_without_discover(self):
+        with pytest.raises(audio_codecs.SelectedCodecNotValid):
+            m = audio_codecs.CodecManager()
+            m.get_encoder('flac')
 
-#     def test_translate_src_to_dest(self):
-#         self.converter.translate_src_to_dest('src/test_file.flac')
+    def test_decoder_list_length_after_discovery(self):
+        m = audio_codecs.CodecManager()
+        m.discover_codecs()
+        l = len(m._avail_decoders)
+        m.discover_codecs()
+        l2 = len(m._avail_decoders)
+        assert l == l2
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_encoder_list_length_after_discovery(self):
+        m = audio_codecs.CodecManager()
+        m.discover_codecs()
+        l = len(m._avail_encoders)
+        m.discover_codecs()
+        l2 = len(m._avail_encoders)
+        assert l == l2
+
+class TestConverter(object):
+    def test_convert_flac_to_mp3(self, flac_converter, codec_manager):
+        flac_converter.Encoder = codec_manager.get_encoder('mp3')
+        flac_converter.start()
+
+    def test_convert_wav_to_mp3(self, wav_converter, codec_manager):
+        wav_converter.Encoder = codec_manager.get_encoder('mp3')
+        wav_converter.start()
+
+    def test_convert_flac_to_fdkaac(self, flac_converter, codec_manager):
+        flac_converter.Encoder = codec_manager.get_encoder('fdkaac')
+        flac_converter.start()
+
+    def test_convert_wav_to_fdkaac(self, wav_converter, codec_manager):
+        wav_converter.Encoder = codec_manager.get_encoder('fdkaac')
+        wav_converter.start()
+
+    def test_convert_flac_to_ogg(self, flac_converter, codec_manager):
+        flac_converter.Encoder = codec_manager.get_encoder('ogg')
+        flac_converter.start()
+
+    def test_convert_wav_to_ogg(self, wav_converter, codec_manager):
+        wav_converter.Encoder = codec_manager.get_encoder('ogg')
+        wav_converter.start()
+
+    def test_convert_invalid_source_dir(self, flac_converter, codec_manager):
+        with pytest.raises(AssertionError):
+            flac_converter.source_dir = None
+            flac_converter.Encoder = codec_manager.get_encoder('mp3')
+            flac_converter.start()
+
+    def test_convert_invalid_destination_dir(self, flac_converter, codec_manager):
+        with pytest.raises(AssertionError):
+            flac_converter.dest_dir = None
+            flac_converter.Encoder = codec_manager.get_encoder('mp3')
+            flac_converter.start()
